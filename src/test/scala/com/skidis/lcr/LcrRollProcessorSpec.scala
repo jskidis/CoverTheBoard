d16050176a7c07d0213c57df8f9a16b74fdf9ac2
jskidis <jason@skidis.com>
2019-01-01 21:58:20 -0600

    RollProcessor and PlayerMap specs were looking very similar since the RollProcessor spec was depending on knowledge of how PlayerMap worked. Introduced mock of PlayerMap in order to break the link. -- I know, a mock is less than ideal, so I'm going to think about what in my design is making a mock necessary here.

--- src/test/scala/com/skidis/lcr/LcrRollProcessorSpec.scala
+++ src/test/scala/com/skidis/lcr/LcrRollProcessorSpec.scala
@@ -1,60 +1,74 @@
 package com.skidis.lcr
 
 import com.skidis.lcr.Direction._
-import com.skidis.lcr.player.{LcrPlayer, LcrPlayerMap, PlayerMap}
+import com.skidis.lcr.player.{LcrPlayer, Player, PlayerMap}
 import org.scalatest.{FunSpec, MustMatchers}
 
 class LcrRollProcessorSpec extends FunSpec with MustMatchers {
-  val (currPlayerName, leftPlayerName, rightPlayerName) = ("Current Player", "Left Player", "Right Player")
-  val (currPlayerItems, leftPlayerItems, rightPlayerItems) = (5, 3, 1)
-  val players = Seq(
-    LcrPlayer(currPlayerName, currPlayerItems),
-    LcrPlayer(leftPlayerName, leftPlayerItems),
-    LcrPlayer(rightPlayerName, rightPlayerItems)
-  )
+
+  val somePlayer = LcrPlayer("Some Player")
+
+  class MockMap(val playerThatLost: Option[Player] = None,
+    val directionLostTo: Option[Direction] = None, val numActions: Int = 0) extends PlayerMap {
+
+    override def playerByName(playerName: String): Option[Player] = Option(somePlayer)
+
+    override def playerLoses(player: Player): PlayerMap = {
+      new MockMap(Some(player), None, numActions +1)
+    }
+    override def playerLosesTo(player: Player, direction: Direction): PlayerMap = {
+      new MockMap(Some(player), Some(direction), numActions +1)
+    }
+
+    override def playerGains(player: Player): PlayerMap = this
+  }
 
   describe("Lcr Turn") {
-    it("if no action is rolled, all player count should remain unchanged") {
-      val playerMap = LcrPlayerMap(players)
-      val player = playerMap.playerByName(currPlayerName).get
+    it("if no action is rolled, no action should be taken on player map") {
+      val playerMap = new MockMap
+      val player = somePlayer
       val rolls = Seq(NoActionDieSide)
 
-      val newMap = LcrRollProcessor.processRolls(player, playerMap, rolls)
-      validateItemCounts(newMap, currPlayerItems, leftPlayerItems, rightPlayerItems)
+      val returnedMap = castMapToMock(LcrRollProcessor.processRolls(player, playerMap, rolls))
+      returnedMap.numActions mustBe 0
+      returnedMap.playerThatLost mustBe None
+      returnedMap.directionLostTo mustBe None
     }
 
-    it("current player count is decremented when Center is rolled") {
-      val playerMap = LcrPlayerMap(players)
-      val player = playerMap.playerByName(currPlayerName).get
+    it("when Center is rolled, player should lose (to pot)") {
+      val playerMap = new MockMap
+      val player = somePlayer
       val rolls = Seq(DirectionDieSide(Center))
 
-      val newMap = LcrRollProcessor.processRolls(player, playerMap, rolls)
-      validateItemCounts(newMap, currPlayerItems -1, leftPlayerItems, rightPlayerItems)
+      val returnedMap = castMapToMock(LcrRollProcessor.processRolls(player, playerMap, rolls))
+      returnedMap.playerThatLost mustBe Option(somePlayer)
+      returnedMap.directionLostTo mustBe None
     }
 
-    it("current player count is decremented and player to right count is incremented when Right is rolled") {
-      val playerMap = LcrPlayerMap(players)
-      val player = playerMap.playerByName(currPlayerName).get
+    it("when Right or Left is rolled, player should lost to player in that direction") {
+      val playerMap = new MockMap
+      val player = somePlayer
       val rolls = Seq(DirectionDieSide(Right))
 
-      val newMap = LcrRollProcessor.processRolls(player, playerMap, rolls)
-      validateItemCounts(newMap, currPlayerItems -1, leftPlayerItems, rightPlayerItems +1)
+      val returnedMap = castMapToMock(LcrRollProcessor.processRolls(player, playerMap, rolls))
+      returnedMap.playerThatLost mustBe Option(somePlayer)
+      returnedMap.directionLostTo mustBe Option(Right)
     }
 
     it("processes multiple rolls") {
-      val playerMap = LcrPlayerMap(players)
-      val player = playerMap.playerByName(currPlayerName).get
+      val playerMap = new MockMap
+      val player = somePlayer
       val rolls = Seq(DirectionDieSide(Right), DirectionDieSide(Right), NoActionDieSide,
         DirectionDieSide(Center), DirectionDieSide(Left), NoActionDieSide)
 
-      val newMap = LcrRollProcessor.processRolls(player, playerMap, rolls)
-      validateItemCounts(newMap, currPlayerItems -4, leftPlayerItems +1, rightPlayerItems +2)
+      val returnedMap = castMapToMock(LcrRollProcessor.processRolls(player, playerMap, rolls))
+      returnedMap.numActions mustBe 4 // don't include no action rolls as they do not impact map
+      returnedMap.playerThatLost mustBe Option(somePlayer) // Mock retains only the last action which should have a Left
+      returnedMap.directionLostTo mustBe Option(Left)
     }
-  }
 
-  def validateItemCounts(playerMap: PlayerMap, playerCount: Int, leftPlayerCount: Int, rightPlayerCount: Int): Unit = {
-    playerMap.playerByName(currPlayerName).get.itemCount mustBe playerCount
-    playerMap.playerByName(leftPlayerName).get.itemCount mustBe leftPlayerCount
-    playerMap.playerByName(rightPlayerName).get.itemCount mustBe rightPlayerCount
+    def castMapToMock(mockMap: PlayerMap): MockMap = mockMap match { case m: MockMap => m }
   }
 }
+
+
--- src/test/scala/com/skidis/lcr/player/PlayerNeighborSpec.scala
+++ src/test/scala/com/skidis/lcr/player/PlayerNeighborSpec.scala
@@ -4,33 +4,59 @@ import com.skidis.lcr.Direction._
 import org.scalatest.{FunSpec, MustMatchers}
 
 class PlayerNeighborSpec extends FunSpec with MustMatchers {
-  val currPlayer = LcrPlayer("Current Player", 5)
-  val leftPlayer = LcrPlayer("Left Player", 3)
-  val rightPlayer = LcrPlayer("Right Player", 1)
-  val players = Seq(currPlayer, leftPlayer, rightPlayer)
+  val playerOne = LcrPlayer("Player 1")
+  val playerTwo = LcrPlayer("Player 2")
+  val playerThree = LcrPlayer("Player 3")
+  val playerFour = LcrPlayer("Player 4")
 
   describe("Player Neighbor") {
     it("returns the proper neighbor to the right and left of a player") {
-      PlayerNeighbor(players)(currPlayer.playerName, Left) mustBe Some(leftPlayer)
-      PlayerNeighbor(players)(currPlayer.playerName, Right) mustBe Some(rightPlayer)
+      val players = Seq(playerOne, playerTwo, playerThree, playerFour)
 
-      PlayerNeighbor(players)(rightPlayer.playerName, Left) mustBe Some(currPlayer)
-      PlayerNeighbor(players)(rightPlayer.playerName, Right) mustBe Some(leftPlayer)
+      PlayerNeighbor(players)(playerOne.playerName, Left) mustBe Some(playerTwo)
+      PlayerNeighbor(players)(playerOne.playerName, Right) mustBe Some(playerFour)
 
-      PlayerNeighbor(players)(leftPlayer.playerName, Left) mustBe Some(rightPlayer)
-      PlayerNeighbor(players)(leftPlayer.playerName, Right) mustBe Some(currPlayer)
+      PlayerNeighbor(players)(playerTwo.playerName, Left) mustBe Some(playerThree)
+      PlayerNeighbor(players)(playerTwo.playerName, Right) mustBe Some(playerOne)
+
+      PlayerNeighbor(players)(playerThree.playerName, Left) mustBe Some(playerFour)
+      PlayerNeighbor(players)(playerThree.playerName, Right) mustBe Some(playerTwo)
+
+      PlayerNeighbor(players)(playerFour.playerName, Left) mustBe Some(playerOne)
+      PlayerNeighbor(players)(playerFour.playerName, Right) mustBe Some(playerThree)
     }
 
     it("returns none if player is not in map") {
+      val players = Seq(playerOne, playerTwo, playerThree)
       val playerNotInMap = LcrPlayer("Some Other Player")
+
       PlayerNeighbor(players)("Some Random Name", Left) mustBe None
       PlayerNeighbor(players)("Some Random Name", Right) mustBe None
     }
 
+    it("handles only having two players") {
+      val players = Seq(playerOne, playerTwo)
+
+      PlayerNeighbor(players)(playerOne.playerName, Left) mustBe Some(playerTwo)
+      PlayerNeighbor(players)(playerOne.playerName, Right) mustBe Some(playerTwo)
+
+      PlayerNeighbor(players)(playerTwo.playerName, Left) mustBe Some(playerOne)
+      PlayerNeighbor(players)(playerTwo.playerName, Right) mustBe Some(playerOne)
+    }
+
+    it("handles only having one player") {
+      val players = Seq(playerOne)
+
+      PlayerNeighbor(players)(playerOne.playerName, Left) mustBe Some(playerOne)
+      PlayerNeighbor(players)(playerOne.playerName, Right) mustBe Some(playerOne)
+    }
+
     it("for grins (maybe useful later, it returns the player if the direction is center") {
-      PlayerNeighbor(players)(currPlayer.playerName, Center) mustBe Some(currPlayer)
-      PlayerNeighbor(players)(leftPlayer.playerName, Center) mustBe Some(leftPlayer)
-      PlayerNeighbor(players)(rightPlayer.playerName, Center) mustBe Some(rightPlayer)
+      val players = Seq(playerOne, playerTwo, playerThree)
+
+      PlayerNeighbor(players)(playerOne.playerName, Center) mustBe Some(playerOne)
+      PlayerNeighbor(players)(playerTwo.playerName, Center) mustBe Some(playerTwo)
+      PlayerNeighbor(players)(playerThree.playerName, Center) mustBe Some(playerThree)
     }
   }
 }
